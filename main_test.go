b2b2705f88124db09d2ebcd78520fc63caabe90d
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2024-11-06 13:41:12 +0000

    Added removed before and after scenerios

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"os"
 	"testing"
@@ -22,6 +23,16 @@ func (f *ComponentTest) InitializeScenario(ctx *godog.ScenarioContext) {
 	if err != nil {
 		panic(err)
 	}
+
+	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
+		component.Reset()
+		return ctx, nil
+	})
+
+	ctx.After(func(ctx context.Context, sc *godog.Scenario, err error) (context.Context, error) {
+		_ = component.Close()
+		return ctx, nil
+	})
 
 	component.RegisterSteps(ctx)
 }
